20d24566f0b852f82084d1d846842ecfcf8874c0
Pride and Prejudice <PapstJL4U@users.noreply.github.com>
2019-05-13 00:58:54 +0200

    can now generate a comparriso betwen all characters and ingame values without stretching

--- guilty_gear_hp_display.py
+++ guilty_gear_hp_display.py
@@ -134,7 +134,7 @@ def linear_health():
 def create_comp_img(character, char_health):
     name = character[0]
     if name != char_health[0]:
-        print (name, char_health)
+        print(name, char_health)
         return "wrong characters"
 
     base = Image.open(os.path.join("img", "healthbar_only_radical.png"), mode='r')
@@ -157,15 +157,16 @@ def create_comp_img(character, char_health):
                   fill=(128, 128, 128, 128), width=3)
 
         draw.text((15, base.height / 3), name, fill=(0, 0, 0, 255), font=fnt)
-        draw.text((5 + base.width - base.width * iip, base.height / 2), str(i)+"%", fill=(0, 0, 0, 255), font=fntsmall)
+        draw.text((5 + base.width - base.width * iip, base.height / 2), str(i) + "%", fill=(0, 0, 0, 255),
+                  font=fntsmall)
         diagonals_start.append((floor(base.width - base.width * iip), base.height))
         i -= 10
         j += 1
 
     base.save(os.path.join("output", "comp", "sub", name + ".png"))
 
-    sum , percentage = 0 ,0
-    for i in range(1,len(char_health)-1):
+    sum, percentage = 0, 0
+    for i in range(1, len(char_health) - 1):
         ehealth = char_health[i]
         percentage += ehealth / char_health[7]
         sum += ehealth
@@ -173,30 +174,95 @@ def create_comp_img(character, char_health):
         draw = ImageDraw.Draw(scaled)
         draw.line([scaled.width * percentage, 0, scaled.width * percentage, scaled.height],
                   fill=(128, 128, 128, 128), width=3)
-        draw.text([scaled.width * percentage - 35, scaled.height/2], str(floor(char_health[i])), \
+        draw.text([scaled.width * percentage - 35, scaled.height / 2], str(floor(char_health[i])), \
                   fill=(0, 0, 0, 255), font=fntsmall)
-        draw.text((15, scaled.height / 2), "EHP appr. "+str(floor(char_health[-1])), fill=(0, 0, 0, 255), font=fntsmall)
+        draw.text((15, scaled.height / 2), "EHP " + str(floor(char_health[-1])), fill=(0, 0, 0, 255),
+                  font=fntsmall)
         diagonals_end.append((floor(scaled.width * percentage), base.height + margin))
 
-    scaled.save(os.path.join("output", "comp", "sub", name +"_scaled"+ ".png"))
+    scaled.save(os.path.join("output", "comp", "sub", name + "_scaled" + ".png"))
 
     comp_image.paste(base, (0, 0))
-    comp_image.paste(scaled,(0,(base.height + margin)))
+    comp_image.paste(scaled, (0, (base.height + margin)))
     for pair in zip(diagonals_start, diagonals_end):
         list = pair[0] + pair[1]
         draw = ImageDraw.Draw(comp_image)
-        draw.line(list , fill=(128, 128, 128, 128), width=3)
-    comp_image.save(os.path.join("output", "comp", name +"_comp"+ ".png"))
+        draw.line(list, fill=(128, 128, 128, 128), width=3)
+    comp_image.save(os.path.join("output", "comp", name + "_comp" + ".png"))
+
 
 def draw_comp():
-
+    """"""
     ch = linear_health()
 
     for i, characters in enumerate(char_list):
         create_comp_img(char_list[i], ch[i])
 
+
+def draw_complete_comp():
+    """generates an image, that shows effective damage compared to the displayed damage"""
+
+    # config values to draw image from
+    decile = characters.HP / 10
+
+    # generate userdata and and dummy, that represents ingame display
+    complete_list = [["Base Value", characters.HP / 2, decile, decile, decile, decile, decile, characters.HP]] + linear_health()
+    complete_list.sort(key=itemgetter(-1))
+
+    #  load base hp bar image and generate font as well as generate final canvas to drawn and paste on
+    base = Image.open(os.path.join("img", "healthbar_only_radical.png"), mode='r')
+    fnt = ImageFont.truetype('C:\Windows\Fonts\chintzy.ttf', 15)
+    fntsmall = ImageFont.truetype('C:\Windows\Fonts\chintzy.ttf', 15)
+    margin = 15
+    width = base.width
+    height = base.height * len(complete_list) + margin * (len(complete_list) -1)
+    comp_image = Image.new(mode="RGBA", size=(width, height))
+
+
+    draw_lines_temp = [] # need this to remember coordinates to draw "diagonals" between each hp bar
+
+    for i, entry in enumerate(complete_list):
+
+        # clean coordinates and paste clean, new HB bar onto the final canvas
+        draw_lines_start, draw_lines_end = [], []
+        comp_image.paste(base, (0, 0+i*(base.height+margin)))
+
+
+        # draw all data of a single character on the new blank hp bar
+        percentage = 0
+        for j in range(1, len(entry) - 1):
+
+            ehealth = entry[j]
+            percentage += ehealth / entry[7]
+            draw = ImageDraw.Draw(comp_image)
+            # draw a line at 50/40/30/20/10%
+            draw.line([comp_image.width * percentage, 0+i*(base.height+margin), comp_image.width * percentage, base.height+0+i*(base.height+margin)],
+                      fill=(128, 128, 128, 128), width=3)
+            # draw eHP values on the hp bar
+            draw.text([width * percentage - 40, base.height / 2 + 0+i*(base.height+margin)], str(floor(entry[j])),fill=(0, 0, 0, 255), font=fntsmall)
+            # draw character name
+            draw.text((15, base.height / 2 +i*(base.height+margin)), entry[0] +" "+ str(floor(entry[-1])), fill=(0, 0, 0, 255),
+                      font=fnt)
+
+            #  generate coordinates for diagonal lines between hb bars
+            draw_lines_start.append((comp_image.width * percentage, base.height+0+i*(base.height+margin)))
+            draw_lines_end.append((comp_image.width * percentage, 0+i*(base.height+margin)))
+
+
+        # draw diagonal hp bars
+        for pair in zip(draw_lines_temp,draw_lines_end):
+            list = pair[0] + pair[1]
+            draw = ImageDraw.Draw(comp_image)
+            draw.line(list, fill=(128, 128, 128, 128), width=3)
+
+        draw_lines_temp = draw_lines_start.copy()
+
+    comp_image.save(os.path.join("output", "all_chars.png"))
+
+
 if __name__ == '__main__':
     # test_stuff()
-    #create_real_hp()
-    #create_real_hp_seperate()
+    create_real_hp()
+    create_real_hp_seperate()
     draw_comp()
+    draw_complete_comp()
