7f86a93f87bee2ec0b5e23a7d1908f781a5102d2
Pride and Prejudice <PapstJL4U@users.noreply.github.com>
2019-05-12 04:53:16 +0200

    added alternative way to get effective HP in effectiveHP_2 I thought something wa wrong.
    You can now create comparision images to see, the "real" damage done.

--- guilty_gear_guts.py
+++ guilty_gear_guts.py
@@ -7,20 +7,22 @@ import plotly.plotly as ply
 import plotly.graph_objs as go
 import guiltygeardata as ggd
 
+
 def generateTrace(charactervalues, xvalues):
     """This function generates all values for a character graph and is based on the plotly web example"""
-    #this is one tuple of 101 values, where [0]=charater name and[1...100]= dmg modifier
+    # this is one tuple of 101 values, where [0]=charater name and[1...100]= dmg modifier
     return go.Scatter(
-        x = xvalues,
-        y = charactervalues[1:],
-        mode = 'lines',
-        name = str(charactervalues[0])
-        )
+        x=xvalues,
+        y=charactervalues[1:],
+        mode='lines',
+        name=str(charactervalues[0])
+    )
+
 
 def main():
     """Can be used to debug other functions as well es generate the plotly graph"""
-    #Generates an offline html if False, otherwise tries to publish the graph online on the plotl website.
-    #Login and PW must be configered according to plotly
+    # Generates an offline html if False, otherwise tries to publish the graph online on the plotl website.
+    # Login and PW must be configered according to plotly
     publishOnline = False
     characters = ggd.characters()
     char_list = characters.char_list
@@ -29,46 +31,47 @@ def main():
     # 2:generates all data and writes it into a txt withto be able compare with manual math :)
     # 3:generates the graph, needs 2 before
     # 4+: stops loop
-    debug=1
+    debug = 1
 
-    while debug<2:
-        if debug==1:
+    while debug < 2:
+        if debug == 1:
             for char in char_list:
                 print('{}s effective HPs are {:.5}'.format(char[0], ggd.effectiveHP(char)))
-            debug+=1
+            debug += 1
 
-        elif debug==2:
+        elif debug == 2:
             try:
-                file=open('guiltygeardata.txt', 'w')
+                file = open('guiltygeardata.txt', 'w')
                 for char in char_list:
                     values = []
                     values.append(char[0])
-                    for i in range(1,101,1):
-                       values.append(ggd.getPercentageDmg(char, i-1))
+                    for i in range(1, 101, 1):
+                        values.append(ggd.getPercentageDmg(char, i - 1))
                     file.write('{}\n'.format(values))
                     alldata.append(values[:])
             except Exception:
                 raise
             finally:
                 file.close()
-                debug+=1
+                debug += 1
 
-        elif debug==3:
-            xv = [i for i in range(0,101)]
-            data=[]
+        elif debug == 3:
+            xv = [i for i in range(0, 101)]
+            data = []
             for char in alldata:
-                trace = generateTrace(char,xv)
+                trace = generateTrace(char, xv)
                 data.append(trace)
-            layout = dict(title ='Guts and Defensive Modifier for Guilty Gear Xrd Rev 2',
-            xaxis = dict(title = 'Health Value in Percentage', mirror = True),
-            yaxis = dict(title = 'Damage Multiplier as an absolute Value' )
-            )
+            layout = dict(title='Guts and Defensive Modifier for Guilty Gear Xrd Rev 2',
+                          xaxis=dict(title='Health Value in Percentage', mirror=True),
+                          yaxis=dict(title='Damage Multiplier as an absolute Value')
+                          )
             fig = dict(data=data, layout=layout)
             if publishOnline:
                 ply.iplot(fig, filename='gg-graph-online')
             else:
                 plotly.offline.plot(fig, filename='gg-graph.html')
-            debug+=1
+            debug += 1
+
 
 if __name__ == '__main__':
     main()
--- guilty_gear_hp_display.py
+++ guilty_gear_hp_display.py
@@ -1,6 +1,7 @@
 from __future__ import division
 from PIL import Image, ImageDraw, ImageFont
 from operator import itemgetter
+from math import floor
 import guiltygeardata as ggd
 import os
 
@@ -13,8 +14,8 @@ hp_end = characters.base_HPbar_in_pixel + 14
 def test_stuff():
     char1 = char_list[11]
     print(ggd.effectiveHP(char1))
-    print( ggd.effectiveHP(char1) / characters.HP)
-    im = Image.open(os.path.join("img","healthbar_only_radical.png"), mode='r')
+    print(ggd.effectiveHP(char1) / characters.HP)
+    im = Image.open(os.path.join("img", "healthbar_only_radical.png"), mode='r')
     im.show()
 
 
@@ -23,90 +24,178 @@ def create_real_hp():
     Base assumption are: unmanipulate health is 420 and is 600pixel in weight at a factor of 1.0
     We stretch the 600pixel weight image according to the factor effectiveHP/baseHP
     """
-    #prepare image manipulation and composition by aggregating key variables
+    # prepare image manipulation and composition by aggregating key variables
     char_size = len(char_list)
-    base_image = Image.open(os.path.join("img","healthbar_only_radical.png"), mode='r')
-    width, height= base_image.size
+    base_image = Image.open(os.path.join("img", "healthbar_only_radical.png"), mode='r')
+    width, height = base_image.size
 
-    #generate stretch factor, find biggest stretch factor and sort data by hp size low->high
+    # generate stretch factor, find biggest stretch factor and sort data by hp size low->high
     biggest_factor = 1.0
     image_data = [("420 base value", 1.0)]
     for char in char_list:
         factor = ggd.effectiveHP(char) / characters.HP
-        image_data.append((char[0],factor))
+        image_data.append((char[0], factor))
         if factor > biggest_factor:
             biggest_factor = factor
 
     image_data.sort(key=itemgetter(1))
 
-    #generate base image and text layer
+    # generate base image and text layer
     #   height = Amount of characters + base value
     #   width = equal to the biggest value of all characters
-    comp_image = Image.new(mode="RGBA",size=(int(biggest_factor*width), height*char_size+1))
-    text_layer = Image.new(mode="RGBA",size=(int(biggest_factor*width), height*char_size+1))
+    comp_image = Image.new(mode="RGBA", size=(int(biggest_factor * width), height * char_size + 1))
+    text_layer = Image.new(mode="RGBA", size=(int(biggest_factor * width), height * char_size + 1))
     text = ImageDraw.Draw(text_layer)
     fnt = ImageFont.truetype('C:\Windows\Fonts\chintzy.ttf', 30)
 
-
-
-    #generate big image by pasting the base_image stretch by character factor and add character name
+    # generate big image by pasting the base_image stretch by character factor and add character name
     offset_pic = 0
-    offset_name = int(height/3)
+    offset_name = int(height / 3)
     for data in image_data:
-        scaled_width=int(data[1]*600)
-        comp_image.paste(base_image.resize((scaled_width, height), Image.ANTIALIAS),(0, offset_pic))
-        text.text((15,offset_name),data[0], fill=(0, 0, 0, 255), font=fnt)
-        offset_name+=height
-        offset_pic+=height
+        scaled_width = int(data[1] * 600)
+        comp_image.paste(base_image.resize((scaled_width, height), Image.ANTIALIAS), (0, offset_pic))
+        text.text((15, offset_name), data[0], fill=(0, 0, 0, 255), font=fnt)
+        offset_name += height
+        offset_pic += height
 
-    #combine text layer and healthbar image and save the combination
+    # combine text layer and healthbar image and save the combination
     output = Image.alpha_composite(comp_image, text_layer)
-    output.save(os.path.join("output","scaled_healthbars.png"))
+    output.save(os.path.join("output", "scaled_healthbars.png"))
 
 
 def create_real_hp_seperate():
-
     """
     Base assumption are: unmanipulate health is 420 and is 600pixel in weight at a factor of 1.0
     We stretch the 600pixel weight image according to the factor effectiveHP/baseHP
     """
-    #prepare image manipulation and composition by aggregating key variables
-    char_size = len(char_list)
-    base_image = Image.open(os.path.join("img","healthbar_only_radical.png"), mode='r')
-    width, height= base_image.size
+    # prepare image manipulation and composition by aggregating key variables
+    base_image = Image.open(os.path.join("img", "healthbar_only_radical.png"), mode='r')
+    width, height = base_image.size
 
-    #generate stretch factor, find biggest stretch factor and sort data by hp size low->high
+    # generate stretch factor, find biggest stretch factor and sort data by hp size low->high
     biggest_factor = 1.0
     image_data = [("420 base value", 1.0)]
     for char in char_list:
         factor = ggd.effectiveHP(char) / characters.HP
-        image_data.append((char[0],factor))
+        image_data.append((char[0], factor))
         if factor > biggest_factor:
             biggest_factor = factor
 
     image_data.sort(key=itemgetter(1))
 
-    #generate base image and text layer
+    # generate base image and text layer
     #   height = Amount of characters + base value
     #   width = equal to the biggest value of all characters
-    comp_image = Image.new(mode="RGBA",size=(int(biggest_factor*width), height))
+    comp_image = Image.new(mode="RGBA", size=(int(biggest_factor * width), height))
 
     fnt = ImageFont.truetype('C:\Windows\Fonts\chintzy.ttf', 30)
 
-    #generate multiple images by pasting the base_image stretch by character factor and add character name
-    offset_name = int(height/3)
+    # generate multiple images by pasting the base_image stretch by character factor and add character name
+    offset_name = int(height / 3)
     for data in image_data:
-        scaled_width=int(data[1]*600)
-        comp_image.paste(base_image.resize((scaled_width, height), Image.ANTIALIAS),(0, 0))
-        text_layer = Image.new(mode="RGBA",size=(int(biggest_factor*600), height))
+        scaled_width = int(data[1] * 600)
+        comp_image.paste(base_image.resize((scaled_width, height), Image.ANTIALIAS), (0, 0))
+        text_layer = Image.new(mode="RGBA", size=(int(biggest_factor * 600), height))
         text = ImageDraw.Draw(text_layer)
-        text.text((15,offset_name),data[0], fill=(0, 0, 0, 255), font=fnt)
-        #combine text layer and healthbar image and save the combination
+        text.text((15, offset_name), data[0], fill=(0, 0, 0, 255), font=fnt)
+        # combine text layer and healthbar image and save the combination
         output = Image.alpha_composite(comp_image, text_layer)
-        output.save(os.path.join("output","single",data[0]+".png"))
+        output.save(os.path.join("output", "single", data[0] + ".png"))
 
+
+def linear_health():
+    """calculate absolute health values for all relevant %hp parts"""
+    health = characters.HP
+    decile = characters.HP / 10
+    # list of format [ NAME, health >=50 health <50, health <40, health <30, health <20, health <10, sum ]
+    char_health = []
+    for char in char_list:
+        name = char[0]
+        highHP = health / 2 / char[2]
+        temp = []
+        temp.append(name)
+        temp.append(highHP)
+
+        hp = highHP
+        guts = char[3]
+        percentage = 55
+        for _ in guts:
+            percentage -= 10
+            temp.append(decile / ggd.getPercentageDmg(char, percentage))
+            hp += decile / ggd.getPercentageDmg(char, percentage)
+
+        temp.append(hp)
+        char_health.append(temp)
+        # print(char[0],hp, ggd.effectiveHP(char), ggd.effectiveHP_2(char))
+    # print(char_health)
+    return char_health
+
+
+def create_comp_img(character, char_health):
+    name = character[0]
+    if name != char_health[0]:
+        print (name, char_health)
+        return "wrong characters"
+
+    base = Image.open(os.path.join("img", "healthbar_only_radical.png"), mode='r')
+    scaled = Image.open(os.path.join("img", "healthbar_only_radical.png"), mode='r')
+    width = max(base.width, scaled.width)
+    margin = 25
+    height = base.height + scaled.height + margin
+    comp_image = Image.new(mode="RGBA", size=(width, height))
+    fnt = ImageFont.truetype('C:\Windows\Fonts\chintzy.ttf', 30)
+    fntsmall = ImageFont.truetype('C:\Windows\Fonts\chintzy.ttf', 15)
+
+    diagonals_start = []
+    diagonals_end = []
+    # draw vertial lines across base image at 50/40/30/20/10%
+    i, j = 50, 0
+    while i > 0:
+        iip = i / 100
+        draw = ImageDraw.Draw(base)
+        draw.line([base.width - base.width * iip, 0, base.width - base.width * iip, base.height],
+                  fill=(128, 128, 128, 128), width=3)
+
+        draw.text((15, base.height / 3), name, fill=(0, 0, 0, 255), font=fnt)
+        draw.text((5 + base.width - base.width * iip, base.height / 2), str(i)+"%", fill=(0, 0, 0, 255), font=fntsmall)
+        diagonals_start.append((floor(base.width - base.width * iip), base.height))
+        i -= 10
+        j += 1
+
+    base.save(os.path.join("output", "comp", "sub", name + ".png"))
+
+    sum , percentage = 0 ,0
+    for i in range(1,len(char_health)-1):
+        ehealth = char_health[i]
+        percentage += ehealth / char_health[7]
+        sum += ehealth
+        # print(ehealth, percentage, sum)
+        draw = ImageDraw.Draw(scaled)
+        draw.line([scaled.width * percentage, 0, scaled.width * percentage, scaled.height],
+                  fill=(128, 128, 128, 128), width=3)
+        draw.text([scaled.width * percentage - 35, scaled.height/2], str(floor(char_health[i])), \
+                  fill=(0, 0, 0, 255), font=fntsmall)
+        diagonals_end.append((floor(scaled.width * percentage), base.height + margin))
+
+    scaled.save(os.path.join("output", "comp", "sub", name +"_scaled"+ ".png"))
+
+    comp_image.paste(base, (0, 0))
+    comp_image.paste(scaled,(0,(base.height + margin)))
+    for pair in zip(diagonals_start, diagonals_end):
+        list = pair[0] + pair[1]
+        draw = ImageDraw.Draw(comp_image)
+        draw.line(list , fill=(255, 255, 255, 128), width=3)
+    comp_image.save(os.path.join("output", "comp", name +"_comp"+ ".png"))
+
+def draw_comp():
+
+    ch = linear_health()
+
+    for i, characters in enumerate(char_list):
+        create_comp_img(char_list[i], ch[i])
 
 if __name__ == '__main__':
-    #test_stuff()
-    create_real_hp()
-    create_real_hp_seperate()
+    # test_stuff()
+    #create_real_hp()
+    #create_real_hp_seperate()
+    draw_comp()
